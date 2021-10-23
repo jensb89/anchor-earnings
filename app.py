5c274627fc78d5fd0f3f9537d6925edffa160458
Jens Brauer <jensb89@users.noreply.github.com>
2021-10-23 16:02:31 +0200

    Corrected deposits

--- app.py
+++ app.py
@@ -7,11 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/address/<id>")
-def anchorErningsForAdress(id):
+@app.route("/address/<address>")
+def anchorErningsForAdress(address):
     # call anchor ...
-    deposits = getAnchorDeposits(id)
+    deposits = getAnchorDeposits(address)
     currentRate = float(getCurrentAUstExchangeRate())
     totalYield = calculateYield(deposits, currentRate)
-    deposits = [{'In': 1075.688451, 'Out': 1158.0, 'fee': 1.672663, 'feeUnit': 'ust', 'time': '2021-08-05T07:08:42Z'}, {'In': 55.831474, 'Out': 59.985, 'fee': 0.549374, 'feeUnit': 'ust', 'time': '2021-08-01T14:02:10Z'}]
-    return render_template('anchorOverview.html', deposits = deposits, address=id, y=totalYield)
+    return render_template('anchorOverview.html', deposits = deposits, address=address, y=totalYield)
