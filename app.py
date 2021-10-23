68e9539e01b28ceb5eb3b8dd2bccbab953adce25
Jens Brauer <jensb89@users.noreply.github.com>
2021-10-24 00:01:14 +0200

    Added yield visualization over time

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from anchorProtocol import getAnchorDeposits, calculateYield, getCurrentAUstExchangeRate
+import requests
+import datetime
 
 app = Flask(__name__)
 
@@ -13,4 +15,41 @@ def anchorErningsForAdress(address):
     deposits = getAnchorDeposits(address)
     currentRate = float(getCurrentAUstExchangeRate())
     totalYield = calculateYield(deposits, currentRate)
-    return render_template('anchorOverview.html', deposits = deposits, address=address, y=totalYield)
+
+    # Add UTC time in s
+    minTime = datetime.datetime.now()
+    for deposit in deposits:
+        deposit["unixTimestamp"] = datetime.datetime.strptime(deposit["time"], "%Y-%m-%dT%H:%M:%SZ")
+        minTime = min(minTime, deposit["unixTimestamp"])
+        deposit["rate"] = deposit["Out"] / deposit["In"]
+    
+    # Graph data
+    histData = getHistData(deposits)
+
+    return render_template('anchorOverview.html', deposits = deposits, address=address, y=totalYield, h=histData)
+
+
+def getHistData(deposits):
+    # we use flipside to get historical aust data. Is there a better way by using terra API directly ??
+    res = requests.get("https://api.flipsidecrypto.com/api/v2/queries/1de96d09-4d77-4ad7-b0c8-e907e86fdcb7/data/latest")
+    res = res.json()
+    histYields = []
+    for elem in res:
+        timeStr = elem["DAYTIMESTAMP"]
+        time = datetime.datetime.strptime(timeStr, "%Y-%m-%dT%H:%M:%SZ")
+        austVal = elem["AUST_VALUE"]
+        
+        startDateReached = True
+        histYield = 0
+        for deposit in deposits:
+            if deposit["unixTimestamp"] > time:
+                continue
+            histYield += (austVal - deposit["rate"]) * deposit["In"]
+            startDateReached = False
+        
+        histYields.append({"time":timeStr, "yield":histYield})
+
+        if startDateReached:
+            return histYields
+    
+    return histYields
